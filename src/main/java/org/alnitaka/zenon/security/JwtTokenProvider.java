1378a2c000aed0cbc765abf68b82a7cc90d9a215
Maxeehs <maxtig49@outlook.fr>
2025-08-29 22:43:47 +0200

    Refactor JwtTokenProvider to use updated JJWT methods for signing and parsing tokens

--- src/main/java/org/alnitaka/zenon/security/JwtTokenProvider.java
+++ src/main/java/org/alnitaka/zenon/security/JwtTokenProvider.java
@@ -2,7 +2,6 @@ package org.alnitaka.zenon.security;
 
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
@@ -27,20 +26,20 @@ public class JwtTokenProvider {
 		Date expiry = new Date(now.getTime() + jwtExpirationMs);
 
 		return Jwts.builder()
-				.setSubject(username)
-				.setIssuedAt(now)
-				.setExpiration(expiry)
-				.signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS512)
+				.subject(username)
+				.issuedAt(now)
+				.expiration(expiry)
+				.signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), Jwts.SIG.HS512)
 				.compact();
 	}
 
 	// 2. Lecture du username
 	public String getUsernameFromJwt(String token) {
 		return Jwts.parser()
-				.setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+				.verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
 				.build()
-				.parseClaimsJws(token)
-				.getBody()
+				.parseSignedClaims(token)
+				.getPayload()
 				.getSubject();
 	}
 
@@ -48,9 +47,9 @@ public class JwtTokenProvider {
 	public boolean validateToken(String token) {
 		try {
 			Jwts.parser()
-					.setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+					.verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
 					.build()
-					.parseClaimsJws(token);
+					.parseSignedClaims(token);
 			return true;
 		} catch (JwtException | IllegalArgumentException e) {
 			 log.warn("JWT invalide", e);
